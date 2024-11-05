886a379416dd496004976a15812e527ba5187589
Joy <joyglory213@gmail.com>
2024-11-06 00:21:29 +0300

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Profile from "./Pages/Profile";
+import Signin from "./Pages/Signin";
+import Signup from "./Pages/Signup";
+import Forgotpassword from "./Pages/Forgot-password";
+import Offers from "./Pages/Offers";
 
 function App() {
-
   return (
     <>
-     
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/profile" element={<Profile/>}></Route>
+          <Route path="/sign-in" element={<Signin/>}></Route>
+          <Route path="/sign-up" element={<Signup/>}></Route>
+          <Route path="/forgot-password" element={<Forgotpassword/>}></Route>
+          <Route path="/offers" element={<Offers/>}></Route>
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
